ec977ec8ade612e6f4c451e89406cf7b29451394
sy <1192052369@qq.com>
2019-05-24 18:01:00 +0800

    add

--- src/pages/home/base.vue
+++ src/pages/home/base.vue
@@ -1,13 +1,15 @@
 <template>
     <div class="home">
-        <span class="title">Tour CQ</span>
-        <mt-search v-model="value">
-            <mt-cell
-                v-for="item in result"
-                :title="item.title"
-                :value="item.value">
-            </mt-cell>
-        </mt-search>
+        <div class="top">
+            <span class="title">Tour CQ</span>
+            <div class="search">
+                <input type="text" palceholder="搜索你感兴趣的~"  v-model="value">
+               
+                <span class="iconfont icon1" v-if="value" @click="delSearch()">&#xe621;</span>
+                <span class="iconfont icon2">&#xe60c;</span>
+            </div>
+        </div>
+        
     </div>
 </template>
 <script>
@@ -15,18 +17,61 @@ export default {
     data () {
         return {
             value: '',
-            result: [
-                {title:'ss',value:'yy'},
-                {title:'ss',value:'yy'},
-                {title:'ss',value:'yy'},
-            ]
+        }
+    },
+    methods: {
+        delSearch(){
+            console.log(1)
+            this.value='';
         }
     }
 }
 </script>
 <style lang="less" scoped>
     @import '../../assets/less/var.less';
-
+    .home {
+        .top{
+            width: 100%;
+            background: @PrimaryColor;
+            height: 10rem;
+            text-align: center;
+            .title{
+                display: inline-block;
+                padding: 2rem 0;
+                
+            }
+            .search{
+                height: auto;
+                width: 80%;
+                margin: 0 auto;
+                padding: 0 !important;
+                position: relative;
+                input{
+                    width: 70%;
+                    height: 1.6rem;
+                    border: none;
+                    border-radius: 1.6rem;
+                    outline:none;
+                    padding:0 3rem 0 1rem;
+                }
+                .icon1{
+                    font-size:1rem;
+                    position: absolute;
+                    right:3rem;
+                    bottom:0;
+                    z-index: 99;
+                }
+                .icon2{
+                    font-size:1rem;
+                    position: absolute;
+                    right:1.5rem;
+                    bottom:0;
+                      z-index: 99;
+                }
+                
+            }
+        }
+    }
 </style>
 
 
